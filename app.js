59bd27d706e286a59231cd32b93b52677a43e3ef
Francisco Berti da Cruz <fbc991@live.com>
2023-10-31 20:04:55 -0300

    done

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express = require('express');
 const app = express();
 
 //rest of the packages
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
 
@@ -43,20 +43,20 @@ app.use(
 );
 app.use(mongoSanitize());
 
-app.use(morgan('tiny'));
+// app.use(morgan('tiny'));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(express.static('./public'));
 app.use(fileUpload());
 
-app.get('/', (req, res) => {
-  res.send('e-commerce api');
-});
-app.get('/api/v1', (req, res) => {
-  // console.log(req.cookies);
-  console.log(req.signedCookies);
-  res.send('e-commerce api');
-});
+// app.get('/', (req, res) => {
+//   res.send('e-commerce api');
+// });
+// app.get('/api/v1', (req, res) => {
+//   // console.log(req.cookies);
+//   console.log(req.signedCookies);
+//   res.send('e-commerce api');
+// });
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/products', productRouter);
